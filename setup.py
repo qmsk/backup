b20549bc9db580855ca96bf60f5c3c387bb0aab9
Tero Marttila <terom@paivola.fi>
2012-02-16 10:14:39 +0200

    setup.py: v0.2 + scripts + author

--- setup.py
+++ setup.py
@@ -2,12 +2,19 @@ from distutils.core import setup
 
 
 setup(
-    name        = 'pvl.backup',
-    version     = '0.1',
+    name            = 'pvl.backup',
+    version         = '0.2',
+
+    url             = 'http://hg.qmsk.net/pvl-backup/',
+    author          = 'Tero Marttila',
+    author_email    = 'terom@paivola.fi',
 
     # code
-    packages    = ['pvl', 'pvl.backup'],
+    packages        = ['pvl', 'pvl.backup'],
 
     # binaries
-    scripts     = ['scripts/pvlbackup-rsync-wrapper'],
+    scripts         = [
+        'scripts/pvlbackup-rsync-wrapper', 
+        'scripts/pvlbackup-rsync-snapshot',
+    ],
 )
