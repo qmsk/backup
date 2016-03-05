a503ed6ab24bdbc46bba1b95d664d5a8989448eb
Tero Marttila <terom@fixme.fi>
2016-03-05 17:34:33 +0200

    setup.py: version 1.0.0-dev

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ from distutils.core import setup
 
 setup(
     name            = 'pvl-backup',
-    version         = '1.0-dev',
+    version         = '1.0.0-dev',
+
     description     = "Päivölä rsync backup utilities",
     url             = 'http://verkko.paivola.fi/hg/pvl-backup/',
     license         = 'MIT',
@@ -12,22 +13,19 @@ setup(
     author          = 'Tero Marttila',
     author_email    = 'terom@paivola.fi',
 
-    # deps
-    install_requires    = [
-        # pvl.args
-        # pvl.invoke
-        'pvl-common >= 0.5.1',
-    ],
- 
-    # lib
-    namespace_packages = [ 'pvl' ],
-    packages        = [
+    namespace_packages = [ 
         'pvl',
+    ],
+    packages = [
         'pvl.backup',
     ],
 
+    install_requires = [
+        'pvl-common~=1.0.0-dev'
+    ],
+ 
     # binaries
-    scripts         = [
+    scripts = [
         'bin/pvl.backup-rsync', 
         'bin/pvl.backup-snapshot',
         'bin/pvl.backup-zfs',
