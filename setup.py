3ec0b06851379de63d0735c6bf2dc24fad17b33c
Tero Marttila <terom@paivola.fi>
2016-12-10 21:44:37 +0200

    version 1.2.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name            = 'pvl-backup',
-    version         = '1.2-dev',
+    version         = '1.2.3',
 
     description     = "Päivölä rsync backup utilities",
     url             = 'http://verkko.paivola.fi/hg/pvl-backup/',
