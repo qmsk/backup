35fe5d7953d8660396236472a4d4ea68b4c3e8f3
Tero Marttila <tero.marttila@funidata.fi>
2020-10-23 18:51:18 +0300

    version 1.5.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name            = 'qmsk-backup',
-    version         = '1.5.0',
+    version         = '1.5.1',
 
     description     = "Automated LVM/ZFS snapshot, rsync backups",
     url             = 'https://github.com/qmsk/backup',
